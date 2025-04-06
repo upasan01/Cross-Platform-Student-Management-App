ab0c392ad069fc3f6c991c56de159b275de2befa
SANJOY <paulsanjoy2923@gmail.com>
2025-04-06 21:28:42 +0530

    input validation and password encryption added

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -9,7 +9,58 @@ const adminRouter = Router()
 
 // SignUp Route
 adminRouter.post("/signup", async (req, res) => {
+    const requireBody = z.object({
+        email: z.string()
+            .min(5)
+            .max(100)
+            .email(),
 
+        password: z.string()
+            .min(8)
+            .max(50)
+            .regex(/[A-Z]/, { message: "Password must contain atleas one uppercase character" })
+            .regex(/[a-z]/, { message: "Password must contain atleas one lowercase character" })
+            .regex(/[0-9]/, { message: "Password must contain atleas one number" })
+            .regex(/[\W_]/, { message: "Password must contain atleas one special character" }),
+
+        firstName: z.string()
+            .min(1)
+            .max(100),
+
+        lastName: z.string()
+            .min(1)
+            .max(100)
+    })
+
+    const parsedData = requireBody.safeParse(req.body)
+
+    if (!parsedData.success) {
+        return res.status(400).json({
+            message: "Incorrect format",
+            error: parsedData.error
+        })
+    }
+
+    const { email, password, firstName, lastName } = req.body
+
+    try {
+        const hashedAdminPassword = await bcrypt.hash(password, 10)
+        await adminModel.create({
+            email: email,
+            password: hashedAdminPassword,
+            firstName: firstName,
+            lastName: lastName
+        })
+    } catch (err) {
+        res.status(409).json({
+            message: "Admin already exists",
+            code: 409
+        })
+    }
+
+    res.json({
+        message: "Admin Signed up"
+    })
 })
 
 // SignIn Route
--- server/routes/user.js
+++ server/routes/user.js
@@ -9,12 +9,46 @@ const userRouter = Router();
 
 // SignUp Route
 userRouter.post("/signup", async (req, res) => {
+    const requireBody = z.object({
+        email: z.string()
+            .min(5)
+            .max(100)
+            .email(),
+
+        password: z.string()
+            .min(8)
+            .max(50)
+            .regex(/[A-Z]/, { message: "Password must contain atleast on uppercase character" })
+            .regex(/[a-z]/, { message: "Password must contain atleast on lowercase character" })
+            .regex(/[0-9]/, { message: "Password must contain atleast on number" })
+            .regex(/[\W_]/, { message: "Password must contain atleast on special character" }),
+
+        firstName: z.string()
+            .min(1)
+            .max(100),
+
+        lastName: z.string()
+            .min(1)
+            .max(100)
+    })
+
+    const parsedData = requireBody.safeParse(req.body)
+
+    if(!parsedData.success){
+        return res.status(400).json({
+            message: "Incorrect format",
+            code: 400
+        })
+    }
+
     const { email, password, firstName, lastName } = req.body
 
     try {
+        const hashedUserPassword = await bcrypt.hash(password, 10)
+
         await userModel.create({
             email: email,
-            password: password,
+            password: hashedUserPassword,
             firstName: firstName,
             lastName: lastName
         })
