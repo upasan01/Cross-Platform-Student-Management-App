f275a5afdd3d97a48ccf45855ba1f1a7f3ed137b
Shubhojit Ghosh <shubhojitghosh.005@gmail.com>
2025-04-10 00:36:08 +0530

    added image picker

--- client/tecb_profiler/lib/components/image_picker.dart
+++ client/tecb_profiler/lib/components/image_picker.dart
@@ -1,7 +1,6 @@
+import 'dart:io'; // For File class
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-
 
 class CustomImagePicker extends StatefulWidget {
   final void Function(String? imagePath) onImagePicked;
@@ -12,22 +11,49 @@ class CustomImagePicker extends StatefulWidget {
   });
 
   @override
-  _CustomImagePickerState createState() => _CustomImagePickerState();
+  CustomImagePickerState createState() => CustomImagePickerState();
 }
 
-class _CustomImagePickerState extends State<CustomImagePicker> {
+class CustomImagePickerState extends State<CustomImagePicker> {
   final ImagePicker _picker = ImagePicker();
   String? _imagePath;
+  final int _maxSizeInBytes = 1 * 1024 * 1024; // 1 MB in bytes
 
   // Function to pick an image from the gallery
   Future<void> _pickImage() async {
     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
     if (image != null) {
-      setState(() {
-        _imagePath = image.path;
-      });
-      widget.onImagePicked(image.path); // Pass the image path to the parent widget
+      final File imageFile = File(image.path);
+      final int imageSizeInBytes = await imageFile.length();
+
+      // Check if image size exceeds 1MB
+      if (imageSizeInBytes > _maxSizeInBytes) {
+        _showImageSizeError();
+      } else {
+        setState(() {
+          _imagePath = image.path;
+        });
+        widget.onImagePicked(image.path); // Pass the image path to the parent widget
+      }
     }
+  }
+
+  // Show a dialog if the image is too large
+  void _showImageSizeError() {
+    showCupertinoDialog(
+      context: context,
+      builder: (context) => CupertinoAlertDialog(
+        title: const Text('Image Too Large'),
+        content: const Text('Please select an image smaller than 1MB.'),
+        actions: [
+          CupertinoDialogAction(
+            isDefaultAction: true,
+            child: const Text('OK'),
+            onPressed: () => Navigator.pop(context),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
