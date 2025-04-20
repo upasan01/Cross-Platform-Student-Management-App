8d2bf6dd8649154638519d2528c1fa703cd5b0e6
Mahir <skmahirashef04@gmail.com>
2025-04-20 20:11:13 +0530

    Fixed Errors

--- WebApp/src/components/FetchDetails.jsx
+++ WebApp/src/components/FetchDetails.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import StudentDetails from './StudentDetails';
 import ParentDetails from './ParentDetails';
 import EducationalDetails from './EducationalDetails';
 import ExtraDetails from './ExtraDetails';
-import { fallback } from './DataDisplayKit';
 import DownloadButton from './DownloadButton';
 
 
