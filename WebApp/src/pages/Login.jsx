b4ae88a37fec7cb592a58b241aa19cb84be5a1e0
Mahir <skmahirashef04@gmail.com>
2025-04-22 15:05:04 +0530

    Fixed login page

--- WebApp/src/pages/Login.jsx
+++ WebApp/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import LoginCard from '../components/LoginCard'
 const Login = () => {
     return (
         <div className="min-h-screen md:bg-clg-bg md:bg-cover w-full flex items-center justify-center bg-gradient-to-br from-[#e0f1ff] to-[#f2f6ff] px-4">
-            <div className="absolute inset-0 bg-black opacity-60 z-0 hidden md:block"></div>
+            <div className="absolute inset-0 bg-black opacity-45 z-0 hidden md:block"></div>
 
             <div className='flex flex-col w-full md:justify-around md:flex-row items-center justify-center z-10 '>
                 <div className='md:text-white text-gray-700 text-center md:w-[55vw]  space-y-4 mt-10 md:mt-0'>
